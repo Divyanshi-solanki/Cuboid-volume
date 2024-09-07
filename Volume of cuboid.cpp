c4b4f38d09d2722614d0c9daf65a0f6d7023cfb5
Divyanshi Solanki <161625343+Divyanshi-solanki@users.noreply.github.com>
2024-09-07 16:58:51 +0530

    Add files via upload

---
+++ Volume of cuboid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float l,b,h;
+   cout<<"Enter the lenght : ";
+   cin>>l;
+   cout<<"Enter the breadth : ";
+   cin>>b;
+   cout<<"Enter the height : ";
+   cin>>h;
+   
+   cout<<"Volume of cuboid is : "<<l*b*h;
+   
+
+    return 0;
+    
+}
+
